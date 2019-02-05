85531f8e9237fca1deaf27b688eb2c6fae462763
Brend7n <45243880+Brend7n@users.noreply.github.com>
2019-02-04 20:02:15 -0500

    Update RobotMap.java

--- robot/RobotMap.java
+++ robot/RobotMap.java
@@ -11,29 +11,33 @@ public class RobotMap {
     public static final int frontRight = 0;
     public static final int middleRight = 1;
     public static final int backRight = 2;
-    public static final int frontLeft = 3;
+    public static final int frontLeft = 6;
     public static final int middleLeft = 4;
     public static final int backLeft = 5;
   
     // PCM Channels
-    public static final int doubleSolenoid1 = 6;
-    public static final int doubleSolenoid2 = 7;
+    public static final int doubleSolenoid1 = 3;
+    public static final int doubleSolenoid2 = 4;
+    public static final int doubleSolenoidShiftLefta = 0;
+    public static final int doubleSolenoidShiftLeftb = 7;
+    public static final int doubleSolenoidShiftRighta = 1;
+    public static final int doubleSolenoidShiftRightb = 6;
+
     // Digital Input Ports
 /*  
-       // Drivetrain Encoder Ports
-  
-      public static final int leftEnc1 = 0;
-      public static final int leftEnc2 = 1;
-      public static final int rightEnc1 = 2;
-      public static final int rightEnc2 = 3;
-   
-      // Intake Ultrasonic Ports
-  
-      public static final int ping = 4;
-      public static final int echo = 5;
-*/
-  // If you are using multiple modules, make sure to define both the port
-  // number and the module. For example you with a rangefinder:
-  // public static int rangefinderPort = 1;
-  // public static int rangefinderModule = 1;
+  // Drivetrain Encoder Ports
+    public static final int leftEnc1 = 0;
+    public static final int leftEnc2 = 1;
+    public static final int rightEnc1 = 2;
+    public static final int rightEnc2 = 3;
+
+  // Intake Ultrasonic Ports
+    public static final int ping = 4;
+    public static final int echo = 5;
+
+  If you are using multiple modules, make sure to define both the port
+  number and the module. For example you with a rangefinder:
+  public static int rangefinderPort = 1;
+  public static int rangefinderModule = 1;
+  */
 }
