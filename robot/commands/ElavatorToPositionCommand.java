91f11043970a17e40d05912b7992f01401e86b7b
Brendan Taylor <45243880+Brend7n@users.noreply.github.com>
2019-03-20 16:58:32 -0400

    Update ElavatorToPositionCommand.java

--- robot/commands/ElavatorToPositionCommand.java
+++ robot/commands/ElavatorToPositionCommand.java
@@ -4,7 +4,6 @@ import edu.wpi.first.wpilibj.command.Command;
 import frc.robot.Robot;
 import frc.robot.RobotMap;
 
-
 public class ElavatorToPositionCommand extends Command {
 
   int targetPos;
@@ -13,12 +12,10 @@ public class ElavatorToPositionCommand extends Command {
   int error;
   public ElavatorToPositionCommand(int targetPos) {
     this.targetPos = targetPos;
-    requires(Robot.elavatorSubsystem);
-    //setTimeout(6);
+    //requires(Robot.elavatorSubsystem);
   }
   @Override
   protected void initialize() {
-    
     if (targetPos == 0) {
       targetPosNum = RobotMap.lowElavatorPosition;
        
@@ -29,38 +26,41 @@ public class ElavatorToPositionCommand extends Command {
     if (targetPos == 2) {
       targetPosNum = RobotMap.highElavatorPosition;
     }
-    
   }
+
   @Override
   protected void execute() {
     error = targetPosNum-Robot.elavatorSubsystem.getEncoderValue();
-    if (error >=RobotMap.highSpeedPositionRange) { desiredPower = 1; }
-
-    else if ( error <RobotMap.highSpeedPositionRange && error > RobotMap.medSpeedPositionRange ) { desiredPower = -1; }
-    else if ( error >RobotMap.medSpeedPositionRange) { desiredPower = 0.7; }
-    else if ( error <RobotMap.medSpeedPositionRange) { desiredPower = -0.7; }
-    else if ( error >RobotMap.lowSpeedPositionRange)  {desiredPower = 0.5; }
-    else if ( error <RobotMap.lowSpeedPositionRange) { desiredPower = -0.5; }
+    if      (error >RobotMap.highSpeedPositionRange ) { desiredPower = -1;  }
+    else if (error <-RobotMap.highSpeedPositionRange) { desiredPower = 1;   }
+    else if (error >RobotMap.medSpeedPositionRange  ) { desiredPower = -0.7;}
+    else if (error <-RobotMap.medSpeedPositionRange ) { desiredPower = 0.7; }
+    else if (error >RobotMap.lowSpeedPositionRange  ) { desiredPower = -0.5;}
+    else if (error <-RobotMap.lowSpeedPositionRange ) { desiredPower = 0.5; }
     else { desiredPower = 0; }
     
-    System.out.println("Power: " + desiredPower);
-    System.out.println("Target distance: " + error );
-    Robot.elavatorSubsystem.elavatorPercentOutput(desiredPower);
+    System.out.println("Power: " + desiredPower + " error: " + error);
+//    System.out.println("targetPosNum: " + targetPosNum);
+//    System.out.println("Robot.elavatorSubsystem.getEncoderValue(): " + Robot.elavatorSubsystem.getEncoderValue());
+//    System.out.println("Distance from Target : " + error );
+//    System.out.println("isFinished() : " + isFinished() );
+Robot.elavatorSubsystem.elavatorPercentOutput(desiredPower);
+
   }
   @Override
   protected boolean isFinished() {
-    //dead band -0.1 < x < 0.1
+    //interrupted when manual, dead band -0.1 < x < 0.1
     if (Robot.m_oi.getOperatorStick().getRawAxis(3) > 0.2 || Robot.m_oi.getOperatorStick().getRawAxis(3) < -0.2) {
       return true;
     } else {
-      if (desiredPower < 0.2 && desiredPower > -0.2) {
+      if (desiredPower == 0) {
         return true;
       } else {
         return false;
       }
     }
-    
   }
+  
   @Override
   protected void end() {
       Robot.elavatorSubsystem.stop();
