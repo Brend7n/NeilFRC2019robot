27b1346e2d9aef0008bc0f51726ec1394d361ae6
Brendan Taylor <45243880+Brend7n@users.noreply.github.com>
2019-03-20 16:52:38 -0400

    Update RobotMap.java

--- robot/RobotMap.java
+++ robot/RobotMap.java
@@ -1,32 +1,6 @@
 package frc.robot;
 
-/**
- * The RobotMap is a mapping from the ports sensors and actuators are wired into
- * to a variable name. This provides flexibility changing wiring, makes checking
- * the wiring easier and significantly reduces the number of magic numbers
- * floating around.
- */
 public class RobotMap {
-    // Drivetrain speed controllers
-    public static final int frontRight = 0;
-    public static final int middleRight = 1;
-    public static final int backRight = 7;
-    public static final int frontLeft = 6;
-    public static final int middleLeft = 4;
-    public static final int backLeft = 5;
-
-    public static final int intake = 2;
-    //can bus ids
-    public static final int elavator = 1;
-  
-    // PCM Channels
-    public static final int doubleSolenoid1 = 1;
-    public static final int doubleSolenoid2 = 5;
-    public static final int doubleSolenoidShiftLefta = 7;
-    public static final int doubleSolenoidShiftLeftb = 0;
-    public static final int doubleSolenoidShiftRighta = 4;
-    public static final int doubleSolenoidShiftRightb = 3;
-
     // elavator constants 4096 is 360 degrees
     // these numbers are not tested yet
     public static final int highElavatorPosition = 20 *4096;
@@ -34,22 +8,26 @@ public class RobotMap {
     public static final int lowElavatorPosition = 5 * 4096;
     public static final int elavatorMax = 50 * 4096;
 
-    public static final double highSpeedPositionRange = 6 * 4096;
-    public static final double medSpeedPositionRange = 4 * 4096;
-    public static final double lowSpeedPositionRange = 0.7 * 4096;
-
-    // Digital Input Ports 
-  // Drivetrain Encoder Ports
-    //public static final int enc1a = 0;
-    //public static final int enc1b = 1;
-/*
-  // Intake Ultrasonic Ports
-    public static final int ping = 4;
-    public static final int echo = 5;
-
-  If you are using multiple modules, make sure to define both the port
-  number and the module. For example you with a rangefinder:
-  public static int rangefinderPort = 1;
-  public static int rangefinderModule = 1;
-  */
+    public static final double highSpeedPositionRange = 1 * 4096;
+    public static final double medSpeedPositionRange = 0.5 * 4096;
+    public static final double lowSpeedPositionRange = 0.1 * 4096;
+    
+    // Drivetrain speed controllers
+    public static final int frontRight = 0;
+    public static final int middleRight = 1;
+    public static final int backRight = 7;
+    public static final int frontLeft = 6;
+    public static final int middleLeft = 4;
+    public static final int backLeft = 5;
+    public static final int intake = 2;
+    //can bus ids
+    public static final int elavator = 1;
+  
+    // PCM Channels
+    public static final int doubleSolenoid1 = 2;
+    public static final int doubleSolenoid2 = 5;
+    public static final int doubleSolenoidShiftLefta = 7;
+    public static final int doubleSolenoidShiftLeftb = 0;
+    public static final int doubleSolenoidShiftRighta = 4;
+    public static final int doubleSolenoidShiftRightb = 3;
 }
